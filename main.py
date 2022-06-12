f6e89642fdd6133fd6e412030baec7563a4ad4ac
Daniel Deri <daniel.derri@gmail.com>
2022-06-12 11:32:02 +0300

    Update Random Forest model and part of main.

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-from knn import print_knn_results
 from naive_bayes import print_gnb_results
+from knn import print_knn_results
+from random_forest import print_rf_results
 from svm import print_svm_results
 
-# print("--------------------------Naive Bayes--------------------------")
-# # print_gnb_results('datasets/breast-cancer.csv')
-# # print_gnb_results('datasets/bank.csv')
-# # print_gnb_results('datasets/acute-nephritis.csv')
-# # print_gnb_results('datasets/acute-inflammation.csv')
-# # print_gnb_results('datasets/blood.csv')
-# # print_gnb_results('datasets/breast-cancer-wisc.csv')
-# # print_gnb_results('datasets/breast-cancer-wisc-diag.csv')
-# # print_gnb_results('datasets/congressional-voting.csv')
-# # print_gnb_results('datasets/chess-krvkp.csv')
-# # print_gnb_results('datasets/breast-cancer-wisc-prog.csv')
-# print_gnb_results()
-#
-# print("--------------------------SVM--------------------------")
-# print_svm_results()
+print("--------------------------Naive Bayes--------------------------")
+print_gnb_results('all_datasets/breast-cancer.csv')
+# print_gnb_results('all_datasets/bank.csv')
+# print_gnb_results('all_datasets/acute-nephritis.csv')
+# print_gnb_results('all_datasets/acute-inflammation.csv')
+# print_gnb_results('all_datasets/blood.csv')
+# print_gnb_results('all_datasets/breast-cancer-wisc.csv')
+# print_gnb_results('all_datasets/breast-cancer-wisc-diag.csv')
+# print_gnb_results('all_datasets/congressional-voting.csv')
+# print_gnb_results('all_datasets/chess-krvkp.csv')
+# print_gnb_results('all_datasets/breast-cancer-wisc-prog.csv')
+
+
+print("--------------------------Random Forest--------------------------")
+print_rf_results('datasets/breast-cancer.csv')
+# print_rf_results('datasets/bank.csv')
+# print_rf_results('datasets/acute-nephritis.csv')
+# print_rf_results('datasets/acute-inflammation.csv')
+# print_rf_results('datasets/blood.csv')
+# print_rf_results('datasets/breast-cancer-wisc.csv')
+# print_rf_results('datasets/breast-cancer-wisc-diag.csv')
+# print_rf_results('datasets/congressional-voting.csv')
+# print_rf_results('datasets/chess-krvkp.csv')
+# print_rf_results('datasets/breast-cancer-wisc-prog.csv')
+
+print("--------------------------SVM--------------------------")
+print_svm_results()
 
 
 print("-------------------------- KNN --------------------------")
---
+++ random_forest.py
@@ -0,0 +1,158 @@
+import time
+import statistics
+from sklearn import metrics
+from sklearn.model_selection import RandomizedSearchCV, KFold, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+
+from functions_module import get_csvs, read_csv, get_x_y_lsts
+
+def get_rf_hyper_parameters(x, y):
+    rf = RandomForestClassifier()
+    params_RF = dict()
+    params_RF['max_features'] = ['auto', 'sqrt']
+    params_RF['bootstrap'] = [True, False]
+    rand_search = GridSearchCV(rf, params_RF, cv=3)
+    results = rand_search.fit(x, y)
+    return results.best_params_
+
+def get_best_rf(x, y):
+    hyper_parameters = get_rf_hyper_parameters(x, y)
+    rf = RandomForestClassifier(bootstrap=hyper_parameters.get("bootstrap"), max_features=hyper_parameters.get("max_features"))
+    return rf
+
+
+def get_metrics(x, y):
+    kf = KFold(n_splits=10, shuffle=True)
+    accuracy_lst = []
+    tpr_lst = []
+    fpr_lst = []
+    precision_lst = []
+    auc_roc_curve_lst = []
+    auc_precision_recall_lst = []
+    training_time_lst = []
+    inference_time_lst = []
+
+    # x_train = []
+    # x_test = []
+    # y_train = []
+    # y_test = []
+    for train_index, test_index in kf.split(x):
+        x_train = [x[i] for i in train_index]
+        x_test = [x[i] for i in test_index]
+        y_train = [y[i] for i in train_index]
+        y_test = [y[i] for i in test_index]
+        rf = get_best_rf(x, y)
+        before_train = time.time()
+        rf.fit(x_train, y_train)
+        after_train = time.time()
+        y_prediction = rf.predict(x_test)
+        # Calculate accuracy
+        accuracy = metrics.accuracy_score(y_test, y_prediction)
+        accuracy_lst.append(accuracy)
+        # Calculate precision
+        precision = metrics.precision_score(y_test, y_prediction)
+        precision_lst.append(precision)
+        # Calculate TPR,FPR
+        conf = confusion_matrix(y_test, y_prediction)
+        tn = conf[0][0]
+        fn = conf[1][0]
+        tp = conf[1][1]
+        fp = conf[0][1]
+        tpr = tp / (tp + fn)
+        fpr = fp / (fp + tn)
+        tpr_lst.append(tpr)
+        fpr_lst.append(fpr)
+
+
+        # Calculate AUC Precision-Recall
+        fpr, tpr, thresh = metrics.roc_curve(y_test, y_prediction)
+        auc_precision_recall = metrics.auc(fpr, tpr)
+        auc_precision_recall_lst.append(auc_precision_recall)
+        # Calculate AUC ROC Curve
+        auc_roc_curve = metrics.roc_auc_score(y_test, y_prediction)
+        auc_roc_curve_lst.append(auc_roc_curve)
+        # Calculate training time
+        training_time_lst.append(after_train-before_train)
+        # Calculate inference time
+        inference_instances = []
+        while len(inference_instances) < 1000:
+            inference_instances.append(x_test[0])
+        before_infer = time.time()
+        rf.predict(inference_instances)
+        after_infer = time.time()
+        inference_time_lst.append(after_infer-before_infer)
+    return accuracy_lst, tpr_lst, fpr_lst, precision_lst, auc_roc_curve_lst, auc_precision_recall_lst, training_time_lst, inference_time_lst
+
+def get_rf_results(csv):
+        x, y = get_x_y_lsts(csv)
+        accuracy_lst, tpr_lst, fpr_lst, precision_lst, auc_roc_curve_lst, auc_precision_recall_lst, training_time_lst, inference_time_lst = get_metrics(x, y)
+        return accuracy_lst, tpr_lst, fpr_lst, precision_lst, auc_roc_curve_lst, auc_precision_recall_lst, training_time_lst, inference_time_lst
+
+# ------------------------------------------------------------------------------------------------------------------------------------
+
+def print_rf_results(csv):
+    print('-----------------------'+csv+'-----------------------')
+    accuracy_lst, tpr_lst, fpr_lst, precision_lst, auc_roc_curve_lst, auc_precision_recall_lst, training_time_lst, inference_time_lst = get_rf_results(csv)
+    print("----Accuracy Results----")
+    print(accuracy_lst)
+    print("----Accuracy Mean----")
+    print(statistics.mean(accuracy_lst))
+    print("----Accuracy std----")
+    print(statistics.stdev(accuracy_lst))
+
+    print("###################################################################")
+
+    print("----TPR Results----")
+    print(tpr_lst)
+    print("----TPR Mean----")
+    print(statistics.mean(tpr_lst))
+    print("----TPR std----")
+    print(statistics.stdev(tpr_lst))
+
+    print("###################################################################")
+
+    print("----FPR Results----")
+    print(fpr_lst)
+    print("----FPR Mean----")
+    print(statistics.mean(fpr_lst))
+    print("----FPR std----")
+    print(statistics.stdev(fpr_lst))
+
+    print("###################################################################")
+
+    print("----AUC ROC Curve Results----")
+    print(auc_roc_curve_lst)
+    print("-----AUC ROC Curve  Mean----")
+    print(statistics.mean(auc_roc_curve_lst))
+    print("-----AUC ROC Curve  std----")
+    print(statistics.stdev(auc_roc_curve_lst))
+
+    print("###################################################################")
+
+    print("----AUC Precision-Recall Results----")
+    print(auc_precision_recall_lst)
+    print("----AUC Precision-Recall Mean----")
+    print(statistics.mean(auc_precision_recall_lst))
+    print("----AUC Precision-Recall std----")
+    print(statistics.stdev(auc_precision_recall_lst))
+
+    print("###################################################################")
+
+    print("----Training Time Results----")
+    print(training_time_lst)
+    print("----Training Time Mean----")
+    print(statistics.mean(training_time_lst))
+    print("----Training Time std----")
+    print(statistics.stdev(training_time_lst))
+
+    print("###################################################################")
+
+    print("----Inference Time Results----")
+    print(inference_time_lst)
+    print("----Inference Time Mean----")
+    print(statistics.mean(inference_time_lst))
+    print("----Inference Time std----")
+    print(statistics.stdev(inference_time_lst))
+
+    print("###################################################################")
